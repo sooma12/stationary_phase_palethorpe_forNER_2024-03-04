7f6dc4088f164862ee9417d0b085963be31109f9
soom12 <soo.m@northeastern.edu>
2024-03-18 10:26:02 -0400

    wrote script to blast srnas vs 17961

---
+++ scripts/sbatch_0_blast_sRNAs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=short
+#SBATCH --job-name=blast_sRNAs
+#SBATCH --time=04:00:00
+#SBATCH -N 1
+#SBATCH -n 2
+#SBATCH --output=/work/geisingerlab/Mark/rnaSeq/stationary_phase_palethorpe_forNER_2024-03-04/logs/%x-%j.output
+#SBATCH --error=/work/geisingerlab/Mark/rnaSeq/stationary_phase_palethorpe_forNER_2024-03-04/logs/%x-%j.error
+#SBATCH --mail-type=END
+#SBATCH --mail-user=soo.m@northeastern.edu
+
+# Files/dirs
+WORK_DIR=/work/geisingerlab/Mark/rnaSeq/stationary_phase_palethorpe_forNER_2024-03-04
+SRNA_FA=${WORK_DIR}/17978-srnas.fasta
+DB_17961=${WORK_DIR}/ref/CP065432
+
+
+# Load conda environment with BLAST
+module load anaconda3/2021.11
+conda activate /work/geisingerlab/conda_env/blast_corr
+
+blastp -query $SRNA_FA -db $DB_17961 \
+    -max_target_seqs 1 \
+    -outfmt 6 -evalue 1e-5 -num_threads 4
