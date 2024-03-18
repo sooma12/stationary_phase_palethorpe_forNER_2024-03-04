015821cbf0cda8da226feb7d6b3fbe4267a195c3
soom12 <soo.m@northeastern.edu>
2024-03-18 10:56:26 -0400

    more bugs

--- scripts/sbatch_0_blast_sRNAs.sh
+++ scripts/sbatch_0_blast_sRNAs.sh
@@ -13,9 +13,9 @@ WORK_DIR=/work/geisingerlab/Mark/rnaSeq/stationary_phase_palethorpe_forNER_2024-
 SRNA_FA=${WORK_DIR}/17978-srnas.fasta
 DB_17961=${WORK_DIR}/ref/CP065432_ref
 
-OUTPUT=${WORK_DIR}/data/sRNA_blast_17961
+OUTDIR=${WORK_DIR}/data/sRNA_blast_17961
 
-mkdir -p $OUTPUT
+mkdir -p $OUTDIR
 
 # Load conda environment with BLAST
 # shellcheck disable=SC2046
@@ -24,4 +24,4 @@ module load ncbi-blast+/2.13.0
 blastn -query $SRNA_FA -db ${DB_17961}/CP065432.1.fasta \
     -max_target_seqs 1 \
     -outfmt 6 -evalue 1e-5 -num_threads 4 \
-    >${OUTPUT}
+    >${OUTDIR}/srna_blast_output.txt
