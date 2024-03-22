04024e3fb6fdd411eb266080dd3233862e5ea22a
soom12 <soo.m@northeastern.edu>
2024-03-22 14:27:07 -0400

    script to run STAR

--- scripts/script_5_prep_sample_sheet_for_STAR.sh
+++ scripts/script_5_prep_sample_sheet_for_STAR.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # prep_sample_sheet_for_starAlign.sh
 # Makes a sample_sheet.txt containing sample ID and R1 and R2 filepaths
-# Assumes each sample file is in the format: WT_1_S84_trimmed_R2.fastq
-# Script uses a split on "_" to grab the sample ID, e.g. WT_1.  Must modify this if sample file names are different!
-# Example output line:  WT_1 /path/to/WT_1_R1.fastq /path/to/WT_1_R2.fastq
+# Assumes each sample file is in the format: SRR16949322_trimmed_R1.fastq
+# Example output line:  SRR16949322 /path/to/SRR16949322_R1.fastq /path/to/SRR16949322_R2.fastq
 # Usage: bash script_5_prep_sample_sheet_for_STAR.sh <path/to/fastqs> <path/to/samplesheet>
 
 SAMPLE_SHEET=${2}
@@ -16,8 +15,7 @@ if [ -f "${SAMPLE_SHEET}" ] ; then
   rm "${SAMPLE_SHEET}"
 fi
 
-# make sample sheet from R1 and R2 files.  Format on each line looks like (space separated):
-# WT_1 /path/to/WT_1_R1.fastq /path/to/WT_1_R2.fastq
+# make sample sheet from R1 and R2 files.
 # from sample sheet, we can access individual items from each line with e.g. `awk '{print $3}' sample_sheet.txt`
 
 paste R1.list R2.list | while read R1 R2 ;
