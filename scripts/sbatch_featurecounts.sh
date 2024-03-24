ed70dffa079fe2baafb023b6132ef8af32ee6253
sooma12 <soo.m@northeastern.edu>
2024-03-24 06:52:44 -0400

    Wrote featurecounts script

---
+++ scripts/sbatch_featurecounts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --partition=short
+#SBATCH --job-name=featureCounts
+#SBATCH --time=02:00:00
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH --output=/work/geisingerlab/Mark/rnaSeq/2024-03_eg_2020_rnaseq/logs/%x-%j.log
+#SBATCH --error=/work/geisingerlab/Mark/rnaSeq/2024-03_eg_2020_rnaseq/logs/%x-%j.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=soo.m@northeastern.edu
+
+# Usage: sbatch sbatch_featurecounts.sh
+BASE_DIR=/work/geisingerlab/Mark/rnaSeq/stationary_phase_palethorpe_forNER_2024-03-04
+OUT_DIR=${BASE_DIR}/data/featurecounts
+# GTF file for sRNAs in 17961
+GTF_REF=${BASE_DIR}/ref/CP065432_1_srnas/srnas_17961_24-03-22.gtf
+STAR_DIR=${BASE_DIR}/data/mapped/
+
+echo "Loading environment and tools"
+module load subread/2.0.6
+
+mkdir -p $OUT_DIR
+
+# Run featureCounts on all BAM files from STAR
+# For detecting sRNAs, need -t flag (indicates what feature in GTF column 3; default is exon.)
+featureCounts \
+-a $GTF_REF \
+-o $OUT_DIR/srna_counts.txt \
+-t sRNA \
+$STAR_DIR/*.bam
